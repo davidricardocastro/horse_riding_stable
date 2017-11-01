78a9d7fbae752b5290511e875771d5031c12cd45
David Castro <davidricardocastro@outlook.com>
2017-11-01 21:09:20 +0100

    added images and some content to pages stable, horses

--- resources/views/stable.blade.php
+++ resources/views/stable.blade.php
@@ -1,5 +1,48 @@
-@extends('wrapper')
+@extends('wrapper') 
 
 @section('content')
-<p>This is the stable.blade.php</p>
+
+
+
+<div class="container">
+    <div class="row">
+
+        <div class="col">
+            <h1> Talli </h1>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+
+            <img src="/img/stable_side.jpg" class="img-fluid" alt="stable side">
+
+        </div>
+
+        <div class="col">
+
+            <p>Talli Andantino toimii Numlahden kartanon historiallisessa ympäristössä. Hevoset asuvat tilavissa karsinoissa
+                vanhassa tiilitallissa. Vapaa- aikansa ne viettävät tarhoissa tai laitumilla ruuna- ja tammalaumoissa. </p>
+        
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+
+            <img src="/img/path.jpg" alt="path">
+
+        </div>
+
+        <div class="col">
+
+            <p>Meillä on käytössämme hyväpohjainen ratsastuskenttä 25x50 metriä, sekä vaihteleva maastoinen laitumia kiertävä
+                maastolenkki.
+            </p>
+
+        </div>
+    </div>
+</div>
+
+
 @endsection
