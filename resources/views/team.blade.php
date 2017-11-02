6aafc2dac1b0a9617e62a8cd41f83f11a74c55e2
David Castro <davidricardocastro@outlook.com>
2017-11-02 16:16:50 +0100

    fixed links for images ./

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -14,7 +14,7 @@
         <div class="card " style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Verdi</h4>
-                <img class="card-img-top" src="/img/horse1_low.jpg" alt="horse">
+                <img class="card-img-top" src="./img/horse1_low.jpg" alt="horse">
                 <p class="card-text">born bla bla bla. parents bla bla bla</p>
             </div>
 
@@ -26,7 +26,7 @@
         <div class="card" style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Roky</h4>
-                <img class="card-img-top" src="/img/horse2_low.jpg" alt="horse">
+                <img class="card-img-top" src="./img/horse2_low.jpg" alt="horse">
                 <p class="card-text">born bla bla bla. parents bla bla bla</p>
             </div>
         </div>
@@ -36,7 +36,7 @@
         <div class="card" style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Antonya</h4>
-                <img class="card-img-top" src="/img/horse3_low.jpg" alt="horse">
+                <img class="card-img-top" src="./img/horse3_low.jpg" alt="horse">
                 <p class="card-text">born bla bla bla. parents bla bla bla</p>
             </div>
         </div>
@@ -46,7 +46,7 @@
         <div class="card" style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Daniel</h4>
-                <img class="card-img-top" src="/img/horse4_low.png" alt="horse">
+                <img class="card-img-top" src="./img/horse4_low.png" alt="horse">
                 <p class="card-text">born bla bla bla. parents bla bla bla</p>
             </div>
         </div>
