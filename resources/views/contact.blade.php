32ef9bc82443c8e8fe020958b07cffbf1f499f9a
David Castro <davidricardocastro@outlook.com>
2017-11-02 17:51:13 +0100

    changed text to finnish in links and added new nav bar to work with bootstrap4

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,61 @@
 @extends('wrapper')
 
+
 @section('content')
-<p>This is the contact.blade.php</p>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-12">
+            <h3>Voit ottaa yhteyttä Andatino Talliin myös oheisella lomakkeella. Täytä yhteystietosi, niin otamme
+                yhteyttä!</h3>
+        </div>
+
+        <div class="col">
+
+
+            <form action="" method="get">
+
+                <div class="form-group">
+                    <label for="Name">Nimi</label>
+                    <input type="text" class="form-control" id="name" placeholder="">
+
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Sähköposti</label>
+                    <input type="email" class="form-control" id="email" placeholder="">
+
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Puhelinnumero</label>
+                    <input type="int" class="form-control" id="phone" placeholder="">
+
+                </div>
+
+
+
+                <div class="form-group">
+                    <label for="message">Viesti</label>
+                    <textarea class="form-control" id="message" rows="6"></textarea>
+
+                </div>
+
+                <button class="btn btn-primary" id: "send_btn" type="submit">
+                    Lähetä
+                </button>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
 @endsection
--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -1,33 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-  
+
     <title> Klaukkala Stable</title>
 </head>
-<body>
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Klaukkala Stable</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="{{ action('ridingController@riding') }}">Riding</a></li>
-            <li><a href="{{ action('stableController@stable') }}">Stable</a></li>
-            <li><a href="{{ action('horseController@horse') }}">Team</a></li>
-            <li><a href="{{ action('contactController@contact') }}">Contact</a></li>
-            <li><a href="{{ action('adminController@admin') }}">User</a></li>
-        </ul>
-    </div>
-</nav>
-@yield('content')
 
-<script src="{{ asset('js/jquery.min.js')}}"></script>
-<script src="{{ asset('js/bootstrap.js') }}"></script>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbar">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Etusivu</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ action('ridingController@riding') }}">Ratsastus</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ action('stableController@stable') }}">Tali</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ action('horseController@horse') }}">Hevoset</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ action('contactController@contact') }}">Yhteydenotto</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ action('adminController@admin') }}">User</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+
+    <script src="{{ asset('js/jquery.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
 
 </body>
+
 </html>
