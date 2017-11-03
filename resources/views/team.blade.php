8d1634a3ea201a2216c240af14534b666c89ee22
David Castro <davidricardocastro@outlook.com>
2017-11-03 15:20:22 +0100

    added padding to cards on the horses page

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -4,36 +4,11 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-12">
+        <div class="col-12 m-3">
             <h1>Hevoset</h1>
-
         </div>
 
-
-
-        <div class="card " style="width: 20rem;">
-            <div class="card-body">
-                <h4 class="card-title">Verdi</h4>
-                <img class="card-img-top" src="./img/horse1_low.jpg" alt="horse">
-                <p class="card-text">born bla bla bla. parents bla bla bla</p>
-            </div>
-
-            
-        </div>
-
-        
-
-        <div class="card" style="width: 20rem;">
-            <div class="card-body">
-                <h4 class="card-title">Roky</h4>
-                <img class="card-img-top" src="./img/horse2_low.jpg" alt="horse">
-                <p class="card-text">born bla bla bla. parents bla bla bla</p>
-            </div>
-        </div>
-
-        
-
-        <div class="card" style="width: 20rem;">
+        <div class="card m-3 text-center bg-light" style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Antonya</h4>
                 <img class="card-img-top" src="./img/horse3_low.jpg" alt="horse">
@@ -41,9 +16,7 @@
             </div>
         </div>
 
-        
-
-        <div class="card" style="width: 20rem;">
+        <div class="card m-3 text-center bg-light" style="width: 20rem;">
             <div class="card-body">
                 <h4 class="card-title">Daniel</h4>
                 <img class="card-img-top" src="./img/horse4_low.png" alt="horse">
@@ -51,10 +24,34 @@
             </div>
         </div>
 
+        <div class="card m-3 text-center bg-light" style="width: 20rem;">
+            <div class="card-body">
+                <h4 class="card-title">Roky</h4>
+                <img class="card-img-top" src="./img/horse2_low.jpg" alt="horse">
+                <p class="card-text">born bla bla bla. parents bla bla bla</p>
+            </div>
+        </div>
+
+        <div class="card m-3 text-center bg-light" style="width: 20rem;">
+            <div class="card-body ">
+                <h4 class="card-title">Verdi</h4>
+                <img class="card-img-top" src="./img/horse1_low.jpg" alt="horse">
+                <p class="card-text">born bla bla bla. parents bla bla bla</p>
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
 
 
     </div>
 </div>
-</div>
+
 
 @endsection
