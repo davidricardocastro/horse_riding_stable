1027552ae3c63866e3069a60439d42d2be7b3121
David Castro <davidricardocastro@outlook.com>
2017-11-03 17:04:12 +0100

    changed contact form and added link on navbar for login/register

--- resources/views/stable.blade.php
+++ resources/views/stable.blade.php
@@ -7,41 +7,39 @@
 <div class="container">
     <div class="row">
 
-        <div class="col">
+        <div class="col m-3">
             <h1> Talli </h1>
         </div>
 
     </div>
-    <div class="row">
-        <div class="col">
+    <div class="row ">
+        <div class="col-sm-12 col-md-4 p-5">
 
             <img src="./img/stable_side_low.jpg" class="img-fluid" alt="stable side">
 
         </div>
 
-        <div class="col">
+        <div class="col-sm-12 col-md-8 p-5 ">
 
-            <h3 class="">Talli Andantino toimii Numlahden kartanon historiallisessa ympäristössä. Hevoset asuvat tilavissa karsinoissa
+             <h3 class="">Talli Andantino toimii Numlahden kartanon historiallisessa ympäristössä. Hevoset asuvat tilavissa karsinoissa
                 vanhassa tiilitallissa. Vapaa- aikansa ne viettävät tarhoissa tai laitumilla ruuna- ja tammalaumoissa. </h3>
-        
+              
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
+    
+        <div class="col-sm-12 col-md-4 p-5">
 
             <img src="./img/path_low.jpg" class="img-fluid" alt="path">
 
         </div>
 
-        <div class="col">
+        <div class="col-sm-12 col-md-8 p-5">
 
             <h3>Meillä on käytössämme hyväpohjainen ratsastuskenttä 25x50 metriä, sekä vaihteleva maastoinen laitumia kiertävä
                 maastolenkki.
             </h3>
 
         </div>
-    </div>
+   
 </div>
 
 
