9ae1564590cd9a4a9d2aa48dede952f33e87b79e
Daniel Palacios <dptorri@gmail.com>
2017-11-07 00:00:16 +0100

    booking mobile
    Draft of the mobile version of the booking. Does not play well with B4

--- prototype calender/booking.php
+++ prototype calender/booking.php
@@ -1,38 +1,25 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery UI Datepicker - Default functionality</title>
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/style_booking.css">  
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="js/jquery-ui.js"></script>
-    <script>
-        $(function () {
-            $("#datepicker").datepicker();
-        });
-    </script>
-
-</head>
-
-<body>
-    
-    <input style="width:100%" type="text" placeholder="tap to select a date..." id="datepicker">
-    <script>
-        $(function () {
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>jQuery UI Datepicker - Default functionality</title>
+<link rel="stylesheet" href="./jquery-ui.css">
+<link rel="stylesheet" href="./style.css">  
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="./jquery-ui.js"></script>
+    <title> Klaukkala Stable</title>
+<script>$(function () {
             $("#accordion").accordion({
                 collapsible: true,
                 heightStyle: "content"
             });
         });
-    </script>
-    </head>
-
-    <body>
-
-        <div id="accordion">
+        $(function () {
+            $("#datepicker").datepicker();
+        });</script>
+            <div id="accordion">
             <h3 class="weekday day1">Monday</h3>
             <table id="table_mobile" style="width:100%">
                 <tr class="row" id="1">
@@ -462,8 +449,236 @@
                 <button type="submit" id="confirm" name="singlebutton" class="btn btn-primary">Confirm</button> 
                 <button type="submit" id="reset" name="singlebutton" class="btn btn-primary">Reset</button> 
             </div>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        td {
+            height: 2rem;
+            width: 12.5%;
+        }
+
+        th {
+            height: 2rem;
+            width: 12.5%;
+        }
+
+        .unchecked {
+            background-color: lightgreen;
+        }
+
+        .weekday {
+            background-color: lightslategray;
+            height: 3em;
+        }
+            @media only screen and (max-width: 768px) {
+            #table_desktop {
+                display: none
+            }
+            @media only screen and (min-width: 769px) {
+            #accordion {
+                display: none
+            }
+        }
+        
+    </style>
+
+</head>
+
+<body>
+
+<?php
+$today = date("D d");
+echo $today;
+
+//$date = strtotime("+1 day");//returns the day+1!
+//echo date('M d, Y', $date);
+?>
+
+<button id="previous_week">Previous Week</button><button id="next_week">Next Week</button>
+    <table id="table_desktop" style="width:100%">
+        <tr class="row" id="0">
+            <th class="slot_title">Time_Slot</th>
+            <th class="weekday day1">Monday 30</th>
+            <th class="weekday day2">Tuesday 31</th>
+            <th class="weekday day3">Wednesday 1</th>
+            <th class="weekday day4">Thursday 2</th>
+            <th class="weekday day5">Friday 3</th>
+            <th class="weekday day6">Saturday 4</th>
+            <th class="weekday day7">Sunday 5</th>
+        </tr>
+        <tr class="row" id="1">
+            <td id="0" class="slot_data">08:00 - 09:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="2">
+            <td id="0" class="slot_data">09:00 - 10:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="3">
+            <td id="0" class="slot_data">10:00 - 11:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="4">
+            <td id="0" class="slot_data">11:00 - 12:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="5">
+            <td id="0" class="slot_data">12:00 - 13:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="6">
+            <td id="0" class="slot_data">13:00 - 14:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="7">
+            <td id="0" class="slot_data">14:00 - 15:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        
+        <tr class="row" id="8">
+            <td id="0" class="slot_data">15:00 - 16:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="9">
+            <td id="0" class="slot_data">16:00 - 17:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="10">
+            <td id="0" class="slot_data">17:00 - 18:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="11">
+            <td id="0" class="slot_data">18:00 - 19:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+        <tr class="row" id="12">
+            <td id="0" class="slot_data">19:00 - 20:00</td>
+            <td id="1" class="slot unchecked" data-class="unchecked"></td>
+            <td id="2" class="slot unchecked" data-class="unchecked"></td>
+            <td id="3" class="slot unchecked" data-class="unchecked"></td>
+            <td id="4" class="slot unchecked" data-class="unchecked"></td>
+            <td id="5" class="slot unchecked" data-class="unchecked"></td>
+            <td id="6" class="slot unchecked" data-class="unchecked"></td>
+            <td id="7" class="slot unchecked" data-class="unchecked"></td>
+        </tr>
+    </table>
+    <div class="legend">Instructions: Please click to book a lesson. Shortly your request will be confirmed by the trainer.</div>
+    <div class="newDiv"></div>
+
+
          
 
-    </body>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+
+
+    //Slot time management and display in the table below the week
+    $('.slot').click(function(){
+        var className = $(this).data('class');//unchecked
+        var parentVal = $(this).parent().attr('id');
+        var elementVal = $(this).attr('id');
+
+        var column_top = $('th').eq(elementVal).text();
+        var row_top = $('tr').eq(parentVal).text();
+
+        console.log(column_top); //Monday, Tuesday!
+        console.log(row_top);//12:00 - 13:00
+
+
+        var className2 = className.slice(2,className.length) //checked by slicing the string
+        $(this).toggleClass(className).toggleClass(className2);
+
+        //we want to display a kickass table with some awesome msg!
+        var newTable = '<p id="tablefor-'+className+'">Lesson for '+
+            column_top+row_top+' was selected</p>';
+
+        //Tell me if this element of the class slot was clicked!
+        if($(this).data("clicked")){
+            //someone clicked so table is already somewhere, lets find it and erase it!
+            $(".newDiv").find("#tablefor-" + className).remove();
+        } else {
+            //this means that the slot has not been clicked so append awesome table
+            $(".newDiv").append(newTable);
+            $(this).append('  selected');
+        }//reverses the data- to register the change on the clicked element
+        $(this).data("clicked", !$(this).data("clicked"));
+     });
+</script>
 
 </html>
