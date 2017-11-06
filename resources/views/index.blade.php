609ff311bcbdd3374f66498c8de93e2090e01e97
Marco F <marco.friso@gmail.com>
2017-11-06 16:42:39 +0100

    map, style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,14 @@
     <h1>Talli</h1>
 </div>
 <div class="container background_light">
-    <img class="img-fluid p-5" src="{{url ('/img/horse5_cut.jpg') }}" alt="horse">
+    <img class="img-fluid p-3" src="{{url ('/img/horse5_cut.jpg') }}" alt="horse">
 </div>
-<div class="container background_light mt-2">
-<h1>Map!</h1>
-
+<div class="container background_light mt-2 p-3">
+    <div class="embed-responsive embed-responsive-16by9" style="max-height:350px">     
+    <div class="mapouter"><div class="gmap_canvas">
+        <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Lopentie 6, 01860 Nurmijärvi, Finland&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>       
+    </div></div>
+    </div>
 </div>
 
 
