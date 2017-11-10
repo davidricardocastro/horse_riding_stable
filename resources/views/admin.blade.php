cafa89e030c49c95a271837e332a6e56be28124f
David Castro <davidricardocastro@outlook.com>
2017-11-10 10:41:37 +0100

    changes to booking table style using bootstrap

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,64 +4,74 @@
     <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style_booking.css') }}">
     <div class="container background_light">
-        <h3>Please select a date</h3>
+       <div class="row">
+       <div class="col-12">
+       
+       <h3>Please select a date</h3>
+       </div>
+            
+        <div class="col-12">
 
         <div id="datepicker"></div>
+        </div>
 
+        <div class="col-12">
         <div id="accordion" style="display:none;">
             <h3 class="weekday day1">Please select a time</h3>
-            <table id="table_mobile" style="width:100%">
-                <tr class="row" id="1">
+            <table class="table" id="table_mobile" style="width:100%">
+                <tr class="" id="1">
                     <td id="0" class="slot_data">08:00 - 09:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked">Gruop(1/4)</td>
                 </tr>
-                <tr class="row" id="2">
+                <tr class="" id="2">
                     <td id="0" class="slot_data">09:00 - 10:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked">Ind 1</td>
                 </tr>
-                <tr class="row" id="3">
+                <tr class="" id="3">
                     <td id="0" class="slot_data">10:00 - 11:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="4">
+                <tr class="" id="4">
                     <td id="0" class="slot_data">11:00 - 12:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="5">
+                <tr class="" id="5">
                     <td id="0" class="slot_data">12:00 - 13:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="6">
+                <tr class="" id="6">
                     <td id="0" class="slot_data">13:00 - 14:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="7">
+                <tr class="" id="7">
                     <td id="0" class="slot_data">14:00 - 15:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="8">
+                <tr class="" id="8">
                     <td id="0" class="slot_data">15:00 - 16:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="9">
+                <tr class="" id="9">
                     <td id="0" class="slot_data">16:00 - 17:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="10">
+                <tr class="" id="10">
                     <td id="0" class="slot_data">17:00 - 18:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="11">
+                <tr class="" id="11">
                     <td id="0" class="slot_data">18:00 - 19:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
-                <tr class="row" id="12">
+                <tr class="" id="12">
                     <td id="0" class="slot_data">19:00 - 20:00</td>
                     <td id="1" class="slot unchecked" data-class="unchecked"></td>
                 </tr>
             </table>
         </div>
+        </div>
 
+        <div class="col">
         <div id="confirm_buttons" style="display:none;" class="confirm">
             <button type="submit" id="confirm" class="btn btn-primary">Confirm</button>
             <button type="submit" id="reset" class="btn btn-alert">Reset</button>
@@ -78,6 +88,9 @@
             <button type="submit" id="back" class="btn btn-primary">Continue</button>
 
         </div>
+        </div>
+    </div>
+
     </div>
 @endsection
 @section('scripts')
