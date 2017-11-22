a5d58e716109528d9357384799d6b4c3bd40ba4c
Marco F <marco.friso@gmail.com>
2017-11-22 21:20:42 +0100

    only some margins

--- resources/views/riding.blade.php
+++ resources/views/riding.blade.php
@@ -10,12 +10,12 @@
     </ul>    
 </div>
 
-<div class="container background_light p-4 mt-2">
+<div class="container background_light pl-4 pr-4 mt-2 pb-4">
     <div class="row">
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-6 pt-4">
             <img src="\img\Num3_low.jpg" class="img-fluid" alt="rider on horse">
         </div>
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-6 pt-4">
             <img src="\img\deearr.jpg" class="img-fluid" alt="rider on horse">
         </div>
     </div>
--- resources/views/stable.blade.php
+++ resources/views/stable.blade.php
@@ -4,16 +4,16 @@
     <p class="m-0">Talli Andantino toimii Numlahden kartanon historiallisessa ympäristössä. Hevoset asuvat tilavissa karsinoissa vanhassa tiilitallissa. Vapaa- aikansa ne viettävät tarhoissa tai laitumilla ruuna- ja tammalaumoissa.</br>
     Meillä on käytössämme hyväpohjainen ratsastuskenttä 25x50 metriä, sekä vaihteleva maastoinen laitumia kiertävä maastolenkki.</p>
 </div>
-<div class="container background_light p-4 mt-2">
+<div class="container background_light pl-4 pr-4 mt-2 pb-4">
     <div class="row">
         <div class="col-12 col-md-4">
-            <img src="./img/path_low.jpg" class="img-fluid" alt="path">
+            <img src="./img/path_low.jpg" class="img-fluid pt-4" alt="path">
         </div>
         <div class="col-12 col-md-4">
-            <img src="./img/stable_side_low.jpg" class="img-fluid" alt="stable side">
+            <img src="./img/stable_side_low.jpg" class="img-fluid pt-4" alt="stable side">
         </div>
         <div class="col-12 col-md-4">
-            <img src="./img/horse_eating2_low.jpg" class="img-fluid" alt="path">
+            <img src="./img/horse_eating2_low.jpg" class="img-fluid pt-4" alt="horse eating2">
         </div>
     </div>
 </div>
