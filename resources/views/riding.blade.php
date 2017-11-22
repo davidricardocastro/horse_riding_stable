67271715402d4b0493ff2d9a4565eb478fc7b928
Marco F <marco.friso@gmail.com>
2017-11-22 19:22:08 +0100

    adjustment to user_data plus minor aesthetic changes

--- resources/views/riding.blade.php
+++ resources/views/riding.blade.php
@@ -2,11 +2,11 @@
 <div class="container background_light p-4">
 <h2>Ratsastus</h2>
 <!--horse riding-->
-    <ul class="m-0">
-        <li>Tarjoamme ratsastustunteja tiistaista lauantaihin, myös aamuisin ja aamupäivisin.</li> 
-        <li>Kaikki tuntimme keskittyvät kouluratsastukseen, muuta teemme myös tarvittaessa harjoituksia puomeilla.</li> 
-        <li>Ryhmien koko max. 4 ratsastajaa.</li>
-        <li>Kaikilla hevosilla on käytössä koulusatula. Hevosten ehdoton maksimi työmäärä oppilaskäytössä on 2 tuntia päivässä.</li>
+    <ul class="m-0 list-group">
+        <li class="list-group-item">Tarjoamme ratsastustunteja tiistaista lauantaihin, myös aamuisin ja aamupäivisin.</li> 
+        <li class="list-group-item">Kaikki tuntimme keskittyvät kouluratsastukseen, muuta teemme myös tarvittaessa harjoituksia puomeilla.</li> 
+        <li class="list-group-item">Ryhmien koko max. 4 ratsastajaa.</li>
+        <li class="list-group-item">Kaikilla hevosilla on käytössä koulusatula. Hevosten ehdoton maksimi työmäärä oppilaskäytössä on 2 tuntia päivässä.</li>
     </ul>    
 </div>
 
@@ -23,12 +23,13 @@
 
 <div class="container background_light p-4 mt-2">
     <h4>Ohjeita ratsastajalle</h4>
-    <h6><strong>Tahdomme säilyttää hevosemme positiivisina ja mielellään työskentelevinä. Huomioithan siis muutamia asioita:</strong></h6>
-    <ul class="m-0">
-        <li>Varaa riittävästi aikaa hevosen harjaamiseen ja varustamiseen ennen tuntia. Vältä kiirettä ja toimi rauhallisesti. Hevosille ei huudeta, eikä niitä kohdella kovakouraisesti.</li>
-        <li>Satulavyön kiristäminen ei saa olla hevoselle epämiellyttävää, teethän sen siis asteittain muiden toimien lomassa. Kylmällä säällä lämmitä kuolaimet huolella ennen hevosen suuhun laittamista.</li>
-        <li>Tunnin jälkeen hoida ratsusi huolella. Kesällä pyyhi hionneet kohdat sienellä. Talvella tai muuten kylmällä ilmalla, peittele hevonen villaviltillä joka löytyy sen karsinan edestä.</li>
-        <li> Hoidamme ja varustamme myös mielellämme hevosen sinulle valmiiksi ennen tuntia ja pois tunnin jälkeen. Ilmoita vain ajoissa tästä tarpeesta!</li>
+    <h6><strong></strong></h6>
+    <ul class="m-0 list-group">
+        <li class="list-group-item list-group-item-success">Tahdomme säilyttää hevosemme positiivisina ja mielellään työskentelevinä. Huomioithan siis muutamia asioita:</li>
+        <li class="list-group-item">Varaa riittävästi aikaa hevosen harjaamiseen ja varustamiseen ennen tuntia. Vältä kiirettä ja toimi rauhallisesti. Hevosille ei huudeta, eikä niitä kohdella kovakouraisesti.</li>
+        <li class="list-group-item">Satulavyön kiristäminen ei saa olla hevoselle epämiellyttävää, teethän sen siis asteittain muiden toimien lomassa. Kylmällä säällä lämmitä kuolaimet huolella ennen hevosen suuhun laittamista.</li>
+        <li class="list-group-item">Tunnin jälkeen hoida ratsusi huolella. Kesällä pyyhi hionneet kohdat sienellä. Talvella tai muuten kylmällä ilmalla, peittele hevonen villaviltillä joka löytyy sen karsinan edestä.</li>
+        <li class="list-group-item"> Hoidamme ja varustamme myös mielellämme hevosen sinulle valmiiksi ennen tuntia ja pois tunnin jälkeen. Ilmoita vain ajoissa tästä tarpeesta!</li>
     </ul>
 </div>
 
