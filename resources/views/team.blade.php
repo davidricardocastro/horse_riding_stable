6e9e0182e83ce3805f732b67e7b6f5a255a8d358
David Castro <davidricardocastro@outlook.com>
2018-11-26 15:59:58 +0200

    added cards with new horses

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -53,8 +53,20 @@
                 </div>
             </div>
             
-            
-
+            <div class="card m-3 text-center bg-light" style="width: 20rem;">
+                <div class="card-body ">
+                    <h4 class="card-title">Rianna</h4>
+                    <img class="card-img-top" src="./img/rianna_low.jpg" alt="Rianna">
+                    <p class="card-text"><!-- info about horse here--></p>
+                </div>
+            </div>
+            <div class="card m-3 text-center bg-light" style="width: 20rem;">
+                <div class="card-body ">
+                    <h4 class="card-title">Robin</h4>
+                    <img class="card-img-top" src="./img/robin_low.jpg" alt="Robin">
+                    <p class="card-text"><!-- info about horse here--></p>
+                </div>
+            </div>
              
     </div>
 </div>
